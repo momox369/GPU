a9cbe04e2c6cd1333e9c483ad8c407c9f18be549
momox369 <salamehmaurice13@gmail.com>
2024-02-21 20:47:25 +0200

    ass4

--- Ass4/kernel.cu
+++ Ass4/kernel.cu
@@ -31,7 +31,7 @@ __global__ void convolution_tiled_kernel(float* input, float* output, unsigned i
             int out_row = in_row + filter_row - FILTER_RADIUS;
             int out_col = in_col + filter_col - FILTER_RADIUS;
 
-            if ((out_row >= 0) && (out_row < height ) && (out_col >= 0) && (out < width ) ) {
+            if ((out_row >= 0) && (out_row < height ) && (out_col >= 0) && (out_col < width ) ) {
                 sum += input_tile[threadIdx.y + filter_row - FILTER_RADIUS][threadIdx.x + filter_col - FILTER_RADIUS] * filter_c[filter_row][filter_col];
             }
         }
