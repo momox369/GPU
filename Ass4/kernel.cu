d52349b10d5fbaa7de2b50bc82b0df5e252d79e1
momox369 <salamehmaurice13@gmail.com>
2024-02-21 17:55:16 +0200

    ass4

--- Ass4/kernel.cu
+++ Ass4/kernel.cu
@@ -9,50 +9,48 @@
 __constant__ float filter_c[FILTER_DIM][FILTER_DIM];
 
 __global__ void convolution_tiled_kernel(float* input, float* output, unsigned int width, unsigned int height) {
+    //Bring the input tile to shared memory
+    _shared_ float input_tile[IN_TILE_DIM][IN_TILE_DIM];
+    int row = threadIdx.y + blockIdx.y * IN_TILE_DIM;
+    int col = threadIdx.x + blockIdx.x * IN_TILE_DIM;
 
-    // TODO
+    if (row >= 0 && row < height && col >= 0 && col < width) {
+        input_tile[threadIdx.y][threadIdx.x] = input[row * width + col];
+    } else {
+        input_tile[threadIdx.y][threadIdx.x] = 0.0f;
+    }
+    __syncthreads();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //Compute the output tile
+    _shared_ float output_tile[OUT_TILE_DIM][OUT_TILE_DIM];
+    if (threadIdx.y < OUT_TILE_DIM && threadIdx.x < OUT_TILE_DIM) {
+        float sum = 0.0f;
+        for (int i = 0; i < FILTER_DIM; i++) {
+            for (int j = 0; j < FILTER_DIM; j++) {
+                sum += filter_c[i][j] * input_tile[i + threadIdx.y][j + threadIdx.x];
+            }
+        }
+        output_tile[threadIdx.y][threadIdx.x] = sum;
+    }
+    __syncthreads();
 
 
 
 }
 
 void copyFilterToGPU(float filter[][FILTER_DIM]) {
-
     // Copy filter to constant memory
-
-    // TODO
-
+    cudaMemcpyToSymbol(filter_c, filter, FILTER_DIM*FILTER_DIM*sizeof(float));
 }
 
 void convolution_tiled_gpu(float* input_d, float* output_d, unsigned int width, unsigned int height) {
+    
+    /*enough threads per block to load an entire input tile,
+      enough blocks in the grid to process every output tile */
 
-    // Call kernel
-
-    // TODO
-
-
-
+    dim3 numThreadsPerBlock(IN_TILE_DIM, IN_TILE_DIM);
+    dim3 numBlocks((width + IN_TILE_DIM - 1)/IN_TILE_DIM, (height + IN_TILE_DIM - 1)/IN_TILE_DIM);
+    convolution_tiled_kernel <<< numBlocks, numThreadsPerBlock >>> (input_d, output_d, width, height);
+    cudaDeviceSynchronize(); 
 }
 
